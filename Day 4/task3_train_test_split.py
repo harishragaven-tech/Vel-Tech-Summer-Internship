402b8c24f38aa50fdcf380d458427b880ac2e6cd
Harish Ragavendar <ragavendarharish382@gmail.com>
2026-06-04 14:16:07 +0530

    Create task3_train_test_split.py

---
+++ Day 4/task3_train_test_split.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+script_dir = Path(__file__).resolve().parent
+csv_path = script_dir / "students_mental_health_survey.csv"
+
+if not csv_path.exists():
+    raise FileNotFoundError(f"CSV file not found: {csv_path}")
+
+
+df = pd.read_csv(csv_path)
+print(df.columns)
+
+X = df[["Age", "Depression_Score", "Anxiety_Score", "Financial_Stress"]]
+
+y = df["Stress_Level"]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42
+)
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+
+print("MSE:", mse)
